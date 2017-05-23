6c10fd2f5f4f292128b67114bfb55f77ab192841
Simon Pfeifhofer <simon.pfeifhofer@gmail.com>
2017-05-23 15:27:53 +0200

    - extend LWM2MObjectBase to support internal-changes (for observations)

--- abstraction/LWM2MObjectBase.cpp
+++ abstraction/LWM2MObjectBase.cpp
@@ -1,7 +1,6 @@
-#include <string.h>
 #include "LWM2MObjectBase.h"
 
-LWM2MObjectBase::LWM2MObjectBase(uint16_t objID) {
+LWM2MObjectBase::LWM2MObjectBase(uint16_t objID) : _lwm2mObject() {
 
     this->_lwm2mObject.objID = objID;
     this->_lwm2mObject.readFunc = &LWM2M_Read;
@@ -11,10 +10,6 @@ LWM2MObjectBase::LWM2MObjectBase(uint16_t objID) {
     this->_lwm2mObject.createFunc = &LWM2M_Create;
     this->_lwm2mObject.deleteFunc = &LWM2M_Delete;
     this->_lwm2mObject.userData = this;
-    this->_lwm2mObject.instanceList = (lwm2m_list_t *)lwm2m_malloc(sizeof(lwm2m_list_t));
-    if (this->_lwm2mObject.instanceList != NULL){
-        memset(this->_lwm2mObject.instanceList, 0, sizeof(lwm2m_list_t));
-    }
 
 }
 
@@ -25,41 +20,87 @@ lwm2m_object_t * LWM2MObjectBase::GetLWM2MObject() {
 uint8_t LWM2MObjectBase::LWM2M_Read(uint16_t instanceId, int * numDataP, lwm2m_data_t ** dataArrayP, lwm2m_object_t * objectP) {
 
 	LWM2MObjectBase* _this = (LWM2MObjectBase*)objectP->userData;
-    return _this->Read(instanceId, numDataP, dataArrayP);
+	return _this->Read(instanceId, numDataP, dataArrayP, objectP);
 
 }
 
 uint8_t LWM2MObjectBase::LWM2M_Write(uint16_t instanceId, int numData, lwm2m_data_t * dataArrayP, lwm2m_object_t * objectP) {
 
 	LWM2MObjectBase* _this = (LWM2MObjectBase*)objectP->userData;
-	return _this->Write(instanceId, numData, dataArrayP);
+	return _this->Write(instanceId, numData, dataArrayP, objectP);
 
 }
 
 uint8_t LWM2MObjectBase::LWM2M_Execute(uint16_t instanceId, uint16_t resourceId, uint8_t * buffer, int length, lwm2m_object_t * objectP) {
 
 	LWM2MObjectBase* _this = (LWM2MObjectBase*)objectP->userData;
-	return _this->Execute(instanceId, resourceId, buffer, length);
+	return _this->Execute(instanceId, resourceId, buffer, length, objectP);
 
 }
 
 uint8_t LWM2MObjectBase::LWM2M_Discover(uint16_t instanceId, int * numDataP, lwm2m_data_t ** dataArrayP, lwm2m_object_t * objectP) {
 
 	LWM2MObjectBase* _this = (LWM2MObjectBase*)objectP->userData;
-	return _this->Discover(instanceId, numDataP, dataArrayP);
+	return _this->Discover(instanceId, numDataP, dataArrayP, objectP);
 
 }
 
 uint8_t LWM2MObjectBase::LWM2M_Create(uint16_t instanceId, int numData, lwm2m_data_t * dataArray, lwm2m_object_t * objectP) {
 
 	LWM2MObjectBase* _this = (LWM2MObjectBase*)objectP->userData;
-	return _this->Create(instanceId, numData, dataArray);
+	return _this->Create(instanceId, numData, dataArray, objectP);
 
 }
 
 uint8_t LWM2MObjectBase::LWM2M_Delete(uint16_t instanceId, lwm2m_object_t * objectP) {
 
 	LWM2MObjectBase* _this = (LWM2MObjectBase*)objectP->userData;
-	return _this->Delete(instanceId);
+	return _this->Delete(instanceId, objectP);
 
+}
+
+uint8_t LWM2MObjectBase::HandleInternalValueChange(lwm2m_data_t * dataArray, lwm2m_object_t * objectP) {
+	return COAP_405_METHOD_NOT_ALLOWED;
+}
+
+uint8_t LWM2MObjectBase::PerformValueChange(lwm2m_context_t * lwm2mH, lwm2m_uri_t * uri, const char * value, size_t valueLength) {
+	
+	lwm2m_object_t * refObject = (lwm2m_object_t *)LWM2M_LIST_FIND(lwm2mH->objectList, uri->objectId);
+	lwm2m_object_t * object = this->GetLWM2MObject();
+	
+	if (object != refObject) {
+		wd_log_error("LWM2MObjectBase -> Object unknown in the given context");
+		return COAP_405_METHOD_NOT_ALLOWED;
+	}
+	
+	if(this->_lwm2mObject.objID != uri->objectId){
+		wd_log_error("LWM2MObjectBase -> Uri doesn't address this object");
+		return COAP_405_METHOD_NOT_ALLOWED;
+	}
+	
+	lwm2m_data_t * dataP;
+	int result;
+
+	dataP = lwm2m_data_new(1);
+	dataP->id = uri->resourceId;
+	lwm2m_data_encode_nstring(value, valueLength, dataP);
+
+	result = this->Write(uri->instanceId, 1, dataP, object);
+	
+	if (result == COAP_405_METHOD_NOT_ALLOWED) {
+		result = this->HandleInternalValueChange(dataP, object);
+	}
+	
+	lwm2m_data_free(1, dataP);
+
+	if (result != COAP_204_CHANGED) {
+		wd_log_error("LWM2MObjectBase -> Failed to change value.");
+		return result;
+	}
+	
+	wd_log_info("LWM2MObjectBase -> Value changed");
+	lwm2m_resource_value_changed(lwm2mH, uri);
+	
+	return result;
+	
 }
--- abstraction/LWM2MObjectBase.h
+++ abstraction/LWM2MObjectBase.h
@@ -3,21 +3,27 @@
 
 extern "C"{
     #include "liblwm2m.h"
+	#include "wd_logging.h"
 }
+
+#include <string.h>
 
 class LWM2MObjectBase{
 
     public:
 		LWM2MObjectBase(uint16_t objID);
         lwm2m_object_t * GetLWM2MObject(void);
+		uint8_t PerformValueChange(lwm2m_context_t * lwm2mH, lwm2m_uri_t * uri, const char * value, size_t valueLength);
 
     protected:
-        virtual uint8_t Read(uint16_t instanceId, int * numDataP, lwm2m_data_t ** dataArrayP) = 0;
-        virtual uint8_t Write(uint16_t instanceId, int numData, lwm2m_data_t * dataArrayP) = 0;
-        virtual uint8_t Execute(uint16_t instanceId, uint16_t resourceId, uint8_t * buffer, int length) = 0;
-        virtual uint8_t Discover(uint16_t instanceId, int * numDataP, lwm2m_data_t ** dataArrayP) = 0;
-        virtual uint8_t Create(uint16_t instanceId, int numData, lwm2m_data_t * dataArray) = 0;
-        virtual uint8_t Delete(uint16_t instanceId) = 0;
+		virtual uint8_t Read(uint16_t instanceId, int * numDataP, lwm2m_data_t ** dataArrayP, lwm2m_object_t * objectP) = 0;
+		virtual uint8_t Write(uint16_t instanceId, int numData, lwm2m_data_t * dataArrayP, lwm2m_object_t * objectP) = 0;
+		virtual uint8_t Execute(uint16_t instanceId, uint16_t resourceId, uint8_t * buffer, int length, lwm2m_object_t * objectP) = 0;
+		virtual uint8_t Discover(uint16_t instanceId, int * numDataP, lwm2m_data_t ** dataArrayP, lwm2m_object_t * objectP) = 0;
+		virtual uint8_t Create(uint16_t instanceId, int numData, lwm2m_data_t * dataArray, lwm2m_object_t * objectP) = 0;
+		virtual uint8_t Delete(uint16_t instanceId, lwm2m_object_t * objectP) = 0;
+	
+		virtual uint8_t HandleInternalValueChange(lwm2m_data_t * dataArray, lwm2m_object_t * objectP);
 
     private:
                 
