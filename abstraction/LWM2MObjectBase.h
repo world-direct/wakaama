e599ceb402aabb19dbcc5d889d1305c23ec1f7cc
Gregor Mayramhof <gregor.mayramhof@world-direct.at>
2019-10-15 17:17:03 +0200

    fixed formating

--- abstraction/LWM2MObjectBase.h
+++ abstraction/LWM2MObjectBase.h
@@ -19,10 +19,8 @@ public:
 protected:
     virtual uint8_t Read(uint16_t instanceId, int *numDataP, lwm2m_data_t **dataArrayP, lwm2m_object_t *objectP) = 0;
     virtual uint8_t Write(uint16_t instanceId, int numData, lwm2m_data_t *dataArrayP, lwm2m_object_t *objectP) = 0;
-    virtual uint8_t
-    Execute(uint16_t instanceId, uint16_t resourceId, uint8_t *buffer, int length, lwm2m_object_t *objectP) = 0;
-    virtual uint8_t
-    Discover(uint16_t instanceId, int *numDataP, lwm2m_data_t **dataArrayP, lwm2m_object_t *objectP) = 0;
+    virtual uint8_t Execute(uint16_t instanceId, uint16_t resourceId, uint8_t *buffer, int length, lwm2m_object_t *objectP) = 0;
+    virtual uint8_t Discover(uint16_t instanceId, int *numDataP, lwm2m_data_t **dataArrayP, lwm2m_object_t *objectP) = 0;
     virtual uint8_t Create(uint16_t instanceId, int numData, lwm2m_data_t *dataArray, lwm2m_object_t *objectP) = 0;
     virtual uint8_t Delete(uint16_t instanceId, lwm2m_object_t *objectP) = 0;
 
@@ -35,10 +33,8 @@ private:
 
     static uint8_t LWM2M_Read(uint16_t instanceId, int *numDataP, lwm2m_data_t **dataArrayP, lwm2m_object_t *objectP);
     static uint8_t LWM2M_Write(uint16_t instanceId, int numData, lwm2m_data_t *dataArrayP, lwm2m_object_t *objectP);
-    static uint8_t
-    LWM2M_Execute(uint16_t instanceId, uint16_t resourceId, uint8_t *buffer, int length, lwm2m_object_t *objectP);
-    static uint8_t
-    LWM2M_Discover(uint16_t instanceId, int *numDataP, lwm2m_data_t **dataArrayP, lwm2m_object_t *objectP);
+    static uint8_t LWM2M_Execute(uint16_t instanceId, uint16_t resourceId, uint8_t *buffer, int length, lwm2m_object_t *objectP);
+    static uint8_t LWM2M_Discover(uint16_t instanceId, int *numDataP, lwm2m_data_t **dataArrayP, lwm2m_object_t *objectP);
     static uint8_t LWM2M_Create(uint16_t instanceId, int numData, lwm2m_data_t *dataArray, lwm2m_object_t *objectP);
     static uint8_t LWM2M_Delete(uint16_t instanceId, lwm2m_object_t *objectP);
 };
